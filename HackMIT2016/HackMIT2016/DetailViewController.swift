1186162e734d39f5e77d1fd9c812ef8a30e5c61b
Nathan Gitter <nathan@nathangitter.com>
2016-09-17 16:29:48 -0400

    added basic structure to home vc

--- HackMIT2016/HackMIT2016/DetailViewController.swift
+++ HackMIT2016/HackMIT2016/DetailViewController.swift
@@ -13,7 +13,23 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = nil // this is a hack to get the swipe back to work
+
+        self.view.backgroundColor = UIColor.black
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
         
     }
     
--- HackMIT2016/HackMIT2016/HomeViewController.swift
+++ HackMIT2016/HackMIT2016/HomeViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let padding: CGFloat = 8
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //
         
     }
+    
+    // MARK: - Collection View Delegate Functions
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let itemDetailVC = UIStoryboard(name: "ItemDetail", bundle: Bundle.main).instantiateViewController(withIdentifier: "DetailItemVC") as! DetailViewController
+        itemDetailVC.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(itemDetailVC, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = (self.collectionView.frame.width - padding * 3) / 2
+        let ratio: CGFloat = 1334 / 750
+        return CGSize(width: cellWidth, height: cellWidth * ratio)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return padding
+    }
+    
 }
