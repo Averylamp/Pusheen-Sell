869b539b9be08556553631a80c9e4c75eb9e9e1e
Avery Lamp <averylamp@gmail.com>
2016-09-18 02:11:11 -0400

    beginning of core motion stuff

--- HackMIT2016/HackMIT2016/HomeViewController.swift
+++ HackMIT2016/HackMIT2016/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
@@ -26,7 +27,56 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
         }
         
     }
+    var isMotionBasedPanEnabled = true
+    let kMovementSmoothing = 0.3
+    let kAnimationDuration = 0.3
+    let kRotationMultiplier = 5.0
+    var motionManager = CMMotionManager()
+    func calculateRotationBasedOnDeviceMotionRotationRate(motion: CMDeviceMotion) {
+        if self.isMotionBasedPanEnabled {
+            var xRotationRate = motion.rotationRate.x
+//            print("Rotation rate - x \(xRotationRate)")
+            var zRotationRate = motion.rotationRate.z
+            var yRotationRate = motion.rotationRate.y
+            
+            if fabs(yRotationRate) > (fabs(xRotationRate)+fabs(zRotationRate)) {
+                var invertedYRotationRate = yRotationRate * -2
+                
+                var interpretedXOffset =  CGFloat(invertedYRotationRate * kRotationMultiplier)
+                var contentOffset = self.clampedContentOffsetForHorizontalOffset(horizontalOffset:  interpretedXOffset)
+                
+                var percentageOffset = contentOffset.x/3300
+//                print("Precent turned \(percentageOffset)")
+//                UIView .animateWithDuration(kMovementSmoothing, delay: 0.0, options: .beginFromCurrentState, animations: { () -> Void in
+//                    self.panningImageView.setContentOffset(CGPointMake(percentageOffset*self.panningImageView.contentSize.width, contentOffset.y), animated: false)
+//                    self.panningScrollView.setContentOffset(contentOffset, animated: false)
+//                    }, completion: nil)
+                
+            }
+            
+        }
+    }
     
+    func clampedContentOffsetForHorizontalOffset(horizontalOffset: CGFloat) -> CGPoint {
+        var maximumXOffset:CGFloat = 1000.0
+//            self.panningScrollView.contentSize.width - CGRectGetWidth(self.panningScrollView.bounds)
+        var minimumXOffset = 0.0
+        var clampedXOffset = fmax(minimumXOffset, Double(fmin(horizontalOffset, maximumXOffset)))
+//        var centeredY = (self.panningScrollView.contentSize.height/2.0) - (CGRectGetHeight(self.panningScrollView.bounds)/2.0)
+        var centeredY: CGFloat = 300
+        return CGPoint(x: CGFloat(clampedXOffset), y: centeredY)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        //        self.panningScrollView.contentOffset = CGPointMake((self.panningScrollView.contentSize.width / 2.0) - (CGRectGetWidth(self.panningScrollView.bounds)) / 2.0, (self.panningScrollView.contentSize.height / 2.0) - (CGRectGetHeight(self.panningScrollView.bounds)) / 2.0)
+        self.motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (motion, error) in
+            self.calculateRotationBasedOnDeviceMotionRotationRate(motion: motion!)
+        }
+//        self.motionManager.startDeviceMotionUpdatesToQueue(OperationQueue.main, withHandler: { (data: CMDeviceMotion!, error: NSError!) -> Void in
+//            self.calculateRotationBasedOnDeviceMotionRotationRate(motion: data)
+//        })
+        
+    }
     func captureButtonAction() {
         let captureVC = UIStoryboard(name: "HomePage", bundle: Bundle.main).instantiateViewController(withIdentifier: "CaptureVC")
         self.present(captureVC, animated: false, completion: nil)
