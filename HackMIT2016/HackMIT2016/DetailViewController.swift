0f336d0f21ac15ceacf800cd005abf2f9787e6e9
Atman Patel <atmanpatel11@dhcp-18-111-117-37.dyn.mit.edu>
2016-09-18 07:29:18 +0530

    detail view controller

--- HackMIT2016/HackMIT2016/DetailViewController.swift
+++ HackMIT2016/HackMIT2016/DetailViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class DetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class DetailViewController: UIViewController{
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
     
     var item: Item?
+    var moviePlayer: MPMoviePlayerController?
+    var tapGesture : UITapGestureRecognizer?
+    var show = true;
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -18,8 +27,13 @@ class DetailViewController: UIViewController, UICollectionViewDelegate, UICollec
         super.viewDidLoad()
         
         self.navigationController?.interactivePopGestureRecognizer?.delegate = nil // this is a hack to get the swipe back to work
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(DetailViewController.toggleBackground))
+        self.view.addGestureRecognizer(tapGesture!)
+        self.perform(#selector(DetailViewController.toggleBackground), with: nil, afterDelay: 4.0)
         
     }
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -34,20 +48,51 @@ class DetailViewController: UIViewController, UICollectionViewDelegate, UICollec
         self.navigationController?.setNavigationBarHidden(false, animated: true)
         
     }
+
+   
     
-    // MARK: - Collection View Delegate Functions
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+    func playVideo() {
+        
+        let videoView = UIView(frame: CGRect(origin: CGPoint(x: self.view.bounds.origin.x,y :self.view.bounds.origin.y), size: CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)))
+        
+        
+        let pathToEx1 = Bundle.main.path(forResource: "myVideoFile", ofType: "mp4")
+        let pathURL = NSURL.fileURL(withPath: pathToEx1!)
+        moviePlayer = MPMoviePlayerController(contentURL: pathURL)
+        if let player = moviePlayer {
+            player.view.frame = videoView.bounds
+            player.prepareToPlay()
+            player.scalingMode = .aspectFill
+            videoView.addSubview(player.view)
+        }
+        
+        self.view.addSubview(videoView)
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemDetailCell", for: indexPath)
-        return cell
+    func toggleBackground(){
+            show = !show
+        if show{
+            showBackground()
+        }else{
+            hideBackground()
+        }
+        
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return collectionView.frame.size
+    func showBackground(){
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(0.25)
+        self.backgroundView.transform = CGAffineTransform.identity
+    
+        UIView.commitAnimations()
+        
+    }
+    
+    func hideBackground(){
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(0.25)
+        self.backgroundView.transform = CGAffineTransform(translationX: 0, y: self.backgroundView.frame.size.height)
+        UIView.commitAnimations()
     }
     
 }
